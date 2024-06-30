cda4ab42b0f0465df702b83f7fa3466e2f6ba65e
naramalkoht <33901227+naramalkoht@users.noreply.github.com>
2024-06-30 11:10:25 +0300

    save model scan

--- src/Traits/HasSeoScore.php
+++ src/Traits/HasSeoScore.php
@@ -4,6 +4,7 @@ namespace Vormkracht10\Seo\Traits;
 
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\DB;
 use Vormkracht10\Seo\Facades\Seo;
 use Vormkracht10\Seo\Models\SeoScore as SeoScoreModel;
 use Vormkracht10\Seo\SeoScore;
@@ -27,11 +28,41 @@ trait HasSeoScore
 
     public function getCurrentScore(): int
     {
-        return $this->seoScore()->getScore();
+        $seo = $this->seoScore();
+        $this->saveScoreToDatabase($seo,$this->url , $this);
+        return $seo->getScore();
     }
 
     public function getCurrentScoreDetails(): array
     {
-        return $this->seoScore()->getScoreDetails();
+        $seo = $this->seoScore();
+        $this->saveScoreToDatabase($seo,$this->url , $this);
+        return $seo->getScoreDetails();
+    }
+    private function saveScoreToDatabase(SeoScore $seo, string $url, ?object $model = null): void
+    {
+        $score = $seo->getScore();
+
+        // Get the failed checks of each score so we can store them in the scan table.
+        $failedChecks = $seo->getFailedChecks()->map(function ($check) {
+            return get_class($check);
+        })->toArray();
+
+        $this->failedChecks = array_unique(array_merge($this->failedChecks, array_values($failedChecks)));
+
+        DB::table('seo_scores')
+            ->insert([
+                'seo_scan_id' => $this->scan->id,
+                'url' => $url,
+                'model_type' => $model ? $model->getMorphClass() : null,
+                'model_id' => $model ? $model->id : null,
+                'score' => $score,
+                'checks' => json_encode([
+                    'failed' => $seo->getFailedChecks(),
+                    'successful' => $seo->getSuccessfulChecks(),
+                ]),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
     }
 }
